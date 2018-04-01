6632538bd1f1a695e2c5f2dd682689937e67f3d8
LeonMulvaney <leonmul96@gmail.com>
2018-04-01 22:24:12 +0100

    Hospital Request section started

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,14 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def hospreq
+  end
+
+  def path
+   data = params[:body]
+   subject=params[:subject]
+   user = params[:email]
+   Mailer.mail_method(data,user,subject).deliver
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get'/static_pages/:hospreq' => "static_pages#hospreq"
 
 end
